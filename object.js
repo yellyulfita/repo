bd83a0fd53d094c1fb07bccdc7508d0be4decd8e
Ultraman tiga <yelliiyulfita@gmail.com>
2024-03-07 09:21:59 +0700

    add blip 2

--- object.js
+++ object.js
@@ -1,5 +1,12 @@
 const ultraman = {
     name: "tiga",
     height : 100,
-    
+    isDead : false,
+    color : "red",
+    blip : "true",
+    weapons : ["sword","shield","lasso"],
+    attack : ()=> {
+    console.log("sword attack")
 }
+}
+
